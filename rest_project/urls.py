eed5b18f1a0e4fe7bb0ad38f3c6dce891be3034e
achaq <abdelkarimachaq@gmail.com>
2019-02-06 13:57:46 +0100

    First version of application moved into github

--- rest_project/urls.py
+++ rest_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('rest_app.urls')),
+    #path('blog/', include('rest_app.urls')),
+    path('', include('rest_app.urls')),
 ]
