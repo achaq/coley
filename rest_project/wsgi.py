beac763a32350c57f25f6206a252097879cbea95
achaq <abdelkarimachaq@gmail.com>
2019-02-15 14:22:40 +0100

    your comment

--- rest_project/wsgi.py
+++ rest_project/wsgi.py
@@ -1,8 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_project.settings')
-
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
