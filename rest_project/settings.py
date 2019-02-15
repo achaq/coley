f5bc08c18727030e78a90fa7f4159ea7558bfcc9
achaq <abdelkarimachaq@gmail.com>
2019-02-15 14:41:22 +0100

    your comment

--- rest_project/settings.py
+++ rest_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    #'django.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
